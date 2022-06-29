e04871dd23037fcb073ed823433d53097ae2a8fa
PBCandoso <pedrobcandoso@gmail.com>
2022-06-29 22:28:18 +0100

    Solved Encryption problem

---
+++ HackerRank/Medium/encryption.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'encryption' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String encryption(String s) {
+        String res = "";
+        String str = s.replace(" ", "");
+        int colls = (int) Math.ceil(Math.sqrt(str.length()));
+        char[] aux = str.toCharArray();
+
+        for (int i = 0; i < colls; i++){
+            for (int j = i; j < str.length(); j+=colls){
+                res+=aux[j];
+            }
+            res+=" ";
+        }
+        
+        
+        return res;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.encryption(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
