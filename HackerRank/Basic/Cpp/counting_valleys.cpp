fc82aefc1856643ab526ba6bb34c904e96229209
Pedro <pedrobcandoso@gmail.com>
2024-01-18 15:56:12 +0000

    Solved Counting Valleys problem in C++

---
+++ HackerRank/Basic/Cpp/counting_valleys.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'countingValleys' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER steps
+ *  2. STRING path
+ */
+
+int countingValleys(int steps, const string &path) {
+
+    int level, valleys = 0;
+
+    for (auto beg = path.cbegin(); beg != path.cend(); ++beg) 
+    {
+        if (*beg == 'D' && level == 0)  {
+            ++valleys;
+        }
+
+        *beg == 'D' ? --level : ++level;
+    }
+    
+    return valleys;
+}
+
+int main()
+{
+
+    string steps_temp;
+    getline(cin, steps_temp);
+
+    int steps = stoi(ltrim(rtrim(steps_temp)));
+
+    string path;
+    getline(cin, path);
+
+    int result = countingValleys(steps, path);
+
+    std::cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
