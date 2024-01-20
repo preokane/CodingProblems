95824d8965e2b389ddeb65cf4f4c33606b0e7bc0
preokane <pedrobcandoso@gmail.com>
2024-01-20 18:47:07 +0000

    Solved Absolute Premutations problem in C++

---
+++ HackerRank/Medium/Cpp/absolute_permutation.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'absolutePermutation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ */
+
+vector<int> absolutePermutation(int n, int k) {
+    vector <int> values;
+
+    for(int i = 1; i<=n; ++i)
+    { 
+        values.push_back(i);
+    }
+
+    for(int i = 0; i < values.size(); ++i)
+    {
+        if(abs(values[i]-i-1) != k)
+        {
+            if(i+k <= n-1)
+            { 
+                int swap = values[i];
+                values[i] = values[i+k];
+                values[i+k] = swap;
+            }
+            else {
+                {return vector<int>({-1});}
+            }
+        } 
+    }
+     
+    return values;
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string first_multiple_input_temp;
+        getline(cin, first_multiple_input_temp);
+
+        vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+        int n = stoi(first_multiple_input[0]);
+
+        int k = stoi(first_multiple_input[1]);
+
+        vector<int> result = absolutePermutation(n, k);
+
+        for (size_t i = 0; i < result.size(); i++) {
+            cout << result[i];
+
+            if (i != result.size() - 1) {
+                cout << " ";
+            }
+        }
+
+        cout << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
