fbc3bb7f2458b21f2e6cd23abd49d2de5ab26e33
preokane <pedrobcandoso@gmail.com>
2024-02-17 17:40:02 +0000

    Solved Pairs problem in C++

---
+++ HackerRank/Medium/Cpp/pairs.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY arr
+ */
+
+int pairs(int k, vector<int> arr) {
+
+    int result = 0;
+    set<int> nums;
+
+    for (int i = 0; i < arr.size(); ++i) {
+        auto ins = nums.insert(arr[i]);
+    }
+
+    for (auto it = nums.cbegin(); it != nums.cend(); ++it)
+    {
+        if (*it < k) continue;
+        if (((*it)-k > 0) && nums.find(*it-k ) != nums.cend()) ++result;
+    }
+
+    return result;
+}
+
+int main()
+{
+    string first_multiple_input_temp;
+    getline(cin, first_multiple_input_temp);
+
+    vector<string> first_multiple_input = split(rtrim(first_multiple_input_temp));
+
+    int n = stoi(first_multiple_input[0]);
+
+    int k = stoi(first_multiple_input[1]);
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = pairs(k, arr);
+
+    cout << result << "\n";
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
